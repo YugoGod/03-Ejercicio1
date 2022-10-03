04f57c9ca67ddcaa4b64e3f37550aded0664046d
Hugo0m <hugo.monton.blanco@cieep.com>
2022-10-03 13:13:31 +0200

    Versión Edu

--- app/src/main/java/com/example/a03_ejercicio1/FraseDespedazada.java
+++ app/src/main/java/com/example/a03_ejercicio1/FraseDespedazada.java
@@ -4,31 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.StringTokenizer;
+
 public class FraseDespedazada extends AppCompatActivity {
+
+    private TextView txtViewFrase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frase_despedazada);
-        
-        visualizaVistas();
+
+        txtViewFrase = findViewById(R.id.txtViewFraseDespedazada);
 
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
+        String frase = bundle.getString("FRASE");
 
         if (bundle != null){
-            if (bundle.containsKey("AZUCAR")){
-                Toast.makeText(this, "Azucar 1 funciona", Toast.LENGTH_SHORT).show();
+            if (bundle.getInt("OPERACION") == R.id.btnContarCarácteresMain){
+                txtViewFrase.setText("Hay " + contarCaracteres(frase) + " carácteres.");
             }else{
-                Toast.makeText(this, "Azucar 2 funciona", Toast.LENGTH_SHORT).show();
+                txtViewFrase.setText("Hay " + contarPalabras(frase) + " palabras.");
             }
-            
         }
-        
     }
 
-    private void visualizaVistas() {
+    private int contarCaracteres(String frase){
+        return frase.trim().length();
+    }
+
+    private int contarPalabras(String frase){
+        StringTokenizer stringTokenizer = new StringTokenizer(frase);
+        return stringTokenizer.countTokens();
     }
 }
--- app/src/main/java/com/example/a03_ejercicio1/MainActivity.java
+++ app/src/main/java/com/example/a03_ejercicio1/MainActivity.java
@@ -7,11 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Atributos para las vistas.
     private EditText txtFrase;
     private Button btnContarPalabras;
     private Button btnContarCaracteres;
+
+    // Constantes
+    public static final int PALABRAS = 1;
+    public static final int CARACTERES = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,44 +26,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         visualizaVistas();
+        // inicializaEventos();
+    }
 
-        btnContarPalabras.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
+    private void inicializaEventos() {
         btnContarCaracteres.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String frase = txtFrase.getText().toString();
-
-                Intent intent = new Intent(MainActivity.this, FraseDespedazada.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("AZUCAR",frase);
-                intent.putExtras(bundle);
-                startActivity(intent);
+                onclick(CARACTERES);
             }
         });
 
         btnContarPalabras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String frase = txtFrase.getText().toString();
-
-                Intent intent = new Intent(MainActivity.this, FraseDespedazada.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("AZUCAR2",frase);
-                intent.putExtras(bundle);
-                startActivity(intent);
+                onclick(PALABRAS);
             }
         });
+    }
+
+    private void onclick(int c ) {
+        String frase = txtFrase.getText().toString();
+
+        if (!frase.trim().isEmpty()){
+            Intent intent = new Intent(MainActivity.this, FraseDespedazada.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("FRASE",frase);
+            bundle.putInt("OPERACION", c);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }else{
+            Toast.makeText(MainActivity.this, "Esta vacío.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void visualizaVistas() {
         txtFrase = findViewById(R.id.txtFraseMain);
         btnContarPalabras = findViewById(R.id.btnContarPalabrasMain);
         btnContarCaracteres = findViewById(R.id.btnContarCarácteresMain);
+    }
+
+    public void onClickBotones(View boton){
+        String frase = txtFrase.getText().toString();
+
+        if (!frase.trim().isEmpty()){
+            Intent intent = new Intent(MainActivity.this, FraseDespedazada.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("FRASE",frase);
+
+            // Cada botón inserta su ID.
+            bundle.putInt("OPERACION", boton.getId());
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }else{
+            Toast.makeText(MainActivity.this, "Esta vacío.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
